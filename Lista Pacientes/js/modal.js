20244297f392d8f8799db93246d7c8a49e9dc49d
tamiris-rtl <146030970+tamiris-rtl@users.noreply.github.com>
2023-11-13 12:47:24 -0300

    arrumando endereco

--- Lista Pacientes/js/modal.js
+++ Lista Pacientes/js/modal.js
@@ -39,16 +39,66 @@ const ModalExcluir = {
 
 
 
+// function exibirDetalhes(row) {
+//     const pacienteId = row.getAttribute('data-id');
+//     const body = document.querySelector('.modal-detalhes-body');
+//     // Agora você pode usar o pacienteId para fazer a requisição à API e exibir os detalhes no modal
+//     // ...
+
+//     // Exemplo de como usar o ID em uma requisição fetch:
+//     fetch(`http://localhost:8080/pacientes/${pacienteId}`)
+//         .then(response => response.json())
+//         .then(paciente => {
+//             const btnExcluir = document.querySelector('#yes');
+
+//             btnExcluir.onclick = function () {
+//                 excluirPaciente(pacienteId);
+//             };
+
+//             body.innerHTML = `
+//             <dl class="lista-detalhes">
+//             <dt>ID</dt>
+//             <dd>${paciente.id}</dd>
+//             <dt>Nome</dt>
+//             <dd>${paciente.nome}</dd>
+//             <dt>E-mail</dt>
+//             <dd>${paciente.email}</dd>
+//             <dt>Telefone</dt>
+//             <dd>${paciente.telefone}</dd>
+//             <dt>CPF</dt>
+//             <dd>${paciente.cpf}</dd>
+//             <dt>Endereço(s) cadastrado(s):</dt>
+//             <dt>Logradouro</dt>
+//             <dd>${paciente.logradouro}</dd>
+//             <dt>Bairro</dt>
+//             <dd>${paciente.bairro}</dd>
+//             <dt>CEP</dt>
+//             <dd>${paciente.cep}</dd>
+//             <dt>Cidade</dt>
+//             <dd>${paciente.cidade}</dd>
+//             <dt>UF</dt>
+//             <dd>${paciente.uf}</dd>
+//             <dt>Complemento</dt>
+//             <dd>${paciente.complemento}</dd>
+//             <dt>Numero</dt>
+//             <dd>${paciente.numero}</dd>
+
+//         </dl> 
+//             `
+//         })
+//         .catch(error => console.error('Erro ao obter detalhes do paciente:', error));
+
+//     Modal.open(); // Abra o modal após obter os detalhes
+// }
+
+
 function exibirDetalhes(row) {
     const pacienteId = row.getAttribute('data-id');
     const body = document.querySelector('.modal-detalhes-body');
-    // Agora você pode usar o pacienteId para fazer a requisição à API e exibir os detalhes no modal
-    // ...
 
-    // Exemplo de como usar o ID em uma requisição fetch:
     fetch(`http://localhost:8080/pacientes/${pacienteId}`)
         .then(response => response.json())
-        .then(paciente => {
+        .then(detalhes => {
             const btnExcluir = document.querySelector('#yes');
 
             btnExcluir.onclick = function () {
@@ -56,27 +106,51 @@ function exibirDetalhes(row) {
             };
 
             body.innerHTML = `
-            <dl class="lista-detalhes">
+                <div class="modal-info-container">
+                    <div class="paciente-info">
+                        <dl class="lista-detalhes">
+                            <dt>ID</dt>
+                            <dd>${detalhes.id}</dd>
+                            <dt>Nome</dt>
+                            <dd>${detalhes.nome}</dd>
+                            <dt>E-mail</dt>
+                            <dd>${detalhes.email}</dd>
+                            <dt>Telefone</dt>
+                            <dd>${detalhes.telefone}</dd>
+                            <dt>CPF</dt>
+                            <dd>${detalhes.cpf}</dd>
+                        </dl>
+                    </div>
 
-            <dt>ID</dt>
-            <dd>${paciente.id}</dd>
-            <dt>Nome</dt>
-            <dd>${paciente.nome}</dd>
-            <dt>E-mail</dt>
-            <dd>${paciente.email}</dd>
-            <dt>Telefone</dt>
-            <dd>${paciente.telefone}</dd>
-            <dt>CPF</dt>
-            <dd>${paciente.cpf}</dd>
+                    <div class="endereco-info">
+                        <dl class="lista-detalhes">
+                            <dt>Endereço(s) cadastrado(s):</dt>
+                            ${detalhes.enderecosDTO.map(endereco => `
+                                <dt>Endereço: </dt>
+                                <dt>Logradouro</dt>
+                                <dd>${endereco.logradouro}</dd>
+                                <dt>Bairro</dt>
+                                <dd>${endereco.bairro}</dd>
+                                <dt>CEP</dt>
+                                <dd>${endereco.cep}</dd>
+                                <dt>Cidade</dt>
+                                <dd>${endereco.cidade}</dd>
+                                <dt>UF</dt>
+                                <dd>${endereco.uf}</dd>
+                                <dt>Complemento</dt>
+                                <dd>${endereco.complemento}</dd>
+                                <dt>Numero</dt>
+                                <dd>${endereco.numero}</dd>
+                            `).join('')}
+                        </dl>
+                    </div>
+                </div>
+            `;
 
-        </dl> 
-            `
+            Modal.open();
         })
         .catch(error => console.error('Erro ao obter detalhes do paciente:', error));
-
-    Modal.open(); // Abra o modal após obter os detalhes
 }
-
 
 
 
